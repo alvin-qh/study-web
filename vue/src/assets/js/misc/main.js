3bb613e15a4c33265bccef6139fbd76400818487
Alvin.Qu <quhao317@163.com>
2020-02-24 21:25:22 +0800

    [misc] add vue-infinite-scroll widget demo

---
+++ vue/src/assets/js/misc/main.js
@@ -0,0 +1,43 @@
+import "../../css/misc/index.less";
+
+import Vue from "vue";
+
+import {runWith} from "../common/common";
+import Router from "vue-router";
+
+import "../widget/breadcrumb";
+import InfiniteScroll from "./vue-infinite-scroll";
+
+
+const routes = [{
+    path: '/vue-infinite-scroll',
+    components: {
+        'default': InfiniteScroll
+    },
+    name: 'InfiniteScroll'
+}];
+
+
+runWith('misc.index', () => {
+    Vue.use(Router);
+
+    const router = new Router({
+        routes,
+        mode: 'hash'
+    });
+
+    new Vue({
+        el: '#app',
+        data() {
+            return {
+                routes
+            };
+        },
+        router,
+        created() {
+            if (router.currentRoute.matched.length === 0) {
+                router.replace(routes[0].path);
+            }
+        }
+    });
+});
