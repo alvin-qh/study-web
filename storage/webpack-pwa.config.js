329e949c4b4a3921d92bd1b56407ff9b2839dfbc
Alvin <quhao317@163.com>
2020-09-29 23:03:24 +0800

    [storage] upgrade package

--- storage/webpack-pwa.config.js
+++ storage/webpack-pwa.config.js
@@ -1,31 +1,33 @@
-import config, {CONFIG} from "./webpack.config";
-import OfflinePlugin from "offline-plugin";
+const config = require('./webpack.config');
+const OfflinePlugin = require('offline-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 config.plugins = config.plugins.concat([
-    new OfflinePlugin({
-        responseStrategy: 'cache-first',
-        AppCache: true,
-        safeToUseOptionalCaches: true,
-        autoUpdate: true,
-        Caches: {
-            main: [
-                '**/*.js',
-                '**/*.css',
-                /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                /\.(woff2?|eot|ttf|otf)(\?.*)?$/
-            ],
-            additional: [':externals:']
-        },
-        externals: [],
-        excludes: ['**/.*', '**/*.map', '**/*.gz', '**/manifest-last.json'],
-        ServiceWorker: {
-            output: 'sw.js',
-            publicPath: '/sw.js',
-            scope: '/',
-            minify: CONFIG.isProd,
-            events: true
-        }
-    })
+  new OfflinePlugin({
+    responseStrategy: 'cache-first',
+    AppCache: true,
+    safeToUseOptionalCaches: true,
+    autoUpdate: true,
+    Caches: {
+      main: [
+        '**/*.js',
+        '**/*.css',
+        /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        /\.(woff2?|eot|ttf|otf)(\?.*)?$/
+      ],
+      additional: [':externals:']
+    },
+    externals: [],
+    excludes: ['**/.*', '**/*.map', '**/*.gz', '**/manifest-last.json'],
+    ServiceWorker: {
+      output: 'sw.js',
+      publicPath: '/sw.js',
+      scope: '/',
+      minify: isProd,
+      events: true
+    }
+  })
 ]);
 
 module.exports = config;
