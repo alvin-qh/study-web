6c36476f1e6bede25f843b32cf1669d0cfc42b5b
Alvin <quhao317@163.com>
2020-09-29 23:47:15 +0800

    [responsive] fix code style

--- responsive/src/assets/js/home/index.js
+++ responsive/src/assets/js/home/index.js
@@ -3,16 +3,16 @@ import "../../css/home/index.less";
 import $ from 'jquery';
 
 $('body').on('click', '.nav.nav-menu-bar>.nav-menu-btn', e => {
-    const $menuBarDropDown = $(e.currentTarget).closest('.nav-menu-bar').find('.nav-menu');
+  const $menuBarDropDown = $(e.currentTarget).closest('.nav-menu-bar').find('.nav-menu');
 
-    const display = $menuBarDropDown.css('display');
-    if ('none' === display) {
-        $menuBarDropDown.css('display', 'block');
-    } else {
-        $menuBarDropDown.css('display', 'none');
-    }
+  const display = $menuBarDropDown.css('display');
+  if ('none' === display) {
+    $menuBarDropDown.css('display', 'block');
+  } else {
+    $menuBarDropDown.css('display', 'none');
+  }
 });
 
 $(window).on('resize', () => {
-    $('.nav.nav-menu-bar>.nav-menu').css('display', '');
+  $('.nav.nav-menu-bar>.nav-menu').css('display', '');
 });
