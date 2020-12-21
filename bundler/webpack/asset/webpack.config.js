4f96b674b99b5e420b9f369c1856f280c14f3c30
quhao <quhao@wisdomgarden.com>
2020-12-21 12:53:55 +0800

    [bundler/webpack] Asset demo

--- bundler/webpack/asset/webpack.config.js
+++ bundler/webpack/asset/webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const toml = require('toml');
 const yaml = require('yamljs');
 const json5 = require('json5');
@@ -14,27 +12,11 @@ module.exports = {
     filename: 'script/[name].js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style/[name].css'
-    })
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // "publicPath" for css file
-              // "file-loader" copy all files into output folder 'dist', and change url relative to 'dist'
-              //      "dist/image/webpack.png" => url("image/webpack.png")
-              // "publicPath: '..'" means add '..' to all file url in css
-              //      "dist/image/webpack.png" => url("../image/webpack.png")
-              publicPath: '..'
-            }
-          },
           {
             loader: 'css-loader'
           }
