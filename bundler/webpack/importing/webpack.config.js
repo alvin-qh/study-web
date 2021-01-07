64e5acfe644a05336ad2ce6a5e7c6a7a391b7f58
quhao <quhao@wisdomgarden.com>
2021-01-07 12:09:46 +0800

    [bundler/webpack] importing, optimize and add comments

--- bundler/webpack/importing/webpack.config.js
+++ bundler/webpack/importing/webpack.config.js
@@ -1,115 +1,50 @@
-const path = require('path');
-
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackConfig = require('./webpack-common.config.js');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development',
-  devtool: 'cheap-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    hot: true,
-    inline: true,
-    stats: 'minimal',
-    compress: true,
-    writeToDisk: true
-  },
-  entry: {
-    'index': './src/script/index.js'
-  },
-  output: {
-    filename: 'script/[name].bundle-[contenthash:8].js',
-    chunkFilename: 'script/[name].chunk-[contenthash:8].js',
-    path: path.resolve(__dirname, 'dist/asset'),
-    pathinfo: false
-  },
+  ...webpackConfig,
   plugins: [
-    new CleanWebpackPlugin({
-      dry: false,
-      cleanStaleWebpackAssets: false,
-      cleanOnceBeforeBuildPatterns: ['../**/*'],
-      dangerouslyAllowCleanPatternsOutsideProject: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style/[name].bundle-[contenthash:8].css'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Importing',
-      template: './src/template/index.html',
-      filename: '../[name].html',
-      chunks: 'all'
-    }),
+    ...webpackConfig.plugins,
+
+    // plugin to analysize bundle composition
     new BundleAnalyzerPlugin({
-      reportFilename: '../bundle-report.html',
-      openAnalyzer: false,
-      generateStatsFile: true,
-      statsFilename: '../stats.json',
-      analyzerMode: 'static'
+      analyzerMode: 'static',   // report saved as 'html' file, also can be 'server', 'static', 'json' or 'disabled'
+      reportFilename: '../bundle-report.html',    // report file name ('analyzerMode' must be 'static')
+      openAnalyzer: false,      // open report file
+      generateStatsFile: true,  // create a statistics file, which is a json file that holds the metadata of the analysis results
+      statsFilename: '../stats.json'  // name of statistics file ('generateStatsFile' must be true)
     })
   ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: '../'
-            }
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(svg|png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10240,
-              name: 'image/[name]-[contenthash:8].[ext]'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(eot|woff|woff2|ttf)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10240,
-              name: 'font/[name]-[contenthash:8].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  },
   optimization: {
     runtimeChunk: 'single',
-    moduleIds: 'deterministic',
+
+    moduleIds: 'deterministic', // tells webpack which algorithm to use when choosing module ids
+                                // set to 'false' (default value) tells webpack that none of built-in algorithms should be used
+                                //    'natural': numeric ids in order of usage.
+                                //    'named': readable ids for better debugging.
+                                //    'deterministic': module names are hashed into small numeric values.
+                                //    'size': numeric ids focused on minimal initial download size.
     splitChunks: {
-      minChunks: 1,
-      minSize: 1,
+      minChunks: 1,     // the minimum times must a module be shared among chunks before splitting
+      minSize: 1,       // minimum size, in bytes, for a chunk to be generated
+
       cacheGroups: {
-        default: false,
+        default: false,       // do not generate default group
+
+        // 'common' group
         common: {
-          chunks: 'initial',
-          name: 'common',
-          reuseExistingChunk: true
+          chunks: 'initial',  // this group for only statically imported module
+          name: 'common',     // chunk name of this group
+          reuseExistingChunk: true   // this group can be reused
         },
+
+        // 'async' group
         async: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: 'async',
-          name: 'async',
-          reuseExistingChunk: true
+          test: /[\\/]node_modules[\\/]/,   // select module from '**/node_modules/' folder
+          chunks: 'async',  // this group for only synchronized imported module
+          name: 'async',    // chunk name of this group
+          reuseExistingChunk: true  // this group can be reused
         }
       }
     }
