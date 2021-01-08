1d30b878ece6f275529f3e01aca003609993504b
quhao <quhao@wisdomgarden.com>
2021-01-08 11:33:24 +0800

    [bundler/webpack] resolve, optimize and add comments

---
+++ bundler/webpack/build/src/script/index.js
@@ -0,0 +1,14 @@
+import '../style/index.css';
+
+function textBox(text) {
+  const $span = document.createElement('span');
+  $span.innerText = text;
+
+  const $div = document.createElement('div');
+  $div.appendChild($span);
+  $div.className = 'text-box';
+  return $div;
+}
+
+const $wrapper = document.querySelector('.main');
+$wrapper.appendChild(textBox('Hello World'));
