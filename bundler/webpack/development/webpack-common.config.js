33c7877a539c602504a6a45aaf5a276457d23802
Alvin <quhao317@163.com>
2023-07-04 22:40:37 +0800

    [webpack] update package.json

--- bundler/webpack/development/webpack-common.config.js
+++ bundler/webpack/development/webpack-common.config.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   output: {
-    path: path.resolve(__dirname, 'dist/asset'),
-    filename: 'script/[name].bundle-[contenthash:8].js',
-    chunkFilename: 'script/[name].chunk-[contenthash:8].js',
+    path: path.resolve(__dirname, "dist/asset"),
+    filename: "script/[name].bundle-[contenthash:8].js",
+    chunkFilename: "script/[name].chunk-[contenthash:8].js",
     pathinfo: false
   },
   plugins: [
     new CleanWebpackPlugin({
       dry: false,
       cleanStaleWebpackAssets: false,
-      cleanOnceBeforeBuildPatterns: ['../**/*'],
+      cleanOnceBeforeBuildPatterns: ["../**/*"],
       dangerouslyAllowCleanPatternsOutsideProject: true
     }),
     new MiniCssExtractPlugin({
-      filename: 'style/[name].bundle-[contenthash:8].css'
+      filename: "style/[name].bundle-[contenthash:8].css"
     }),
     new HtmlWebpackPlugin({
-      title: 'Development',
-      template: './src/template/index.html',
-      filename: '../[name].html',
-      chunks: 'all'
+      title: "Development",
+      template: "./src/template/index.html",
+      filename: "../[name].html",
+      chunks: "all"
     })
   ],
   module: {
@@ -34,7 +34,7 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             cacheDirectory: true,
             cacheCompression: false
@@ -47,11 +47,11 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../'
+              publicPath: "../"
             }
           },
           {
-            loader: 'css-loader'
+            loader: "css-loader"
           }
         ]
       },
@@ -59,10 +59,10 @@ module.exports = {
         test: /\.(svg|png|jpg|gif)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10240,
-              name: 'image/[name]-[contenthash:8].[ext]'
+              name: "image/[name]-[contenthash:8].[ext]"
             }
           }
         ]
@@ -71,10 +71,10 @@ module.exports = {
         test: /\.(eot|woff|woff2|ttf)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10240,
-              name: 'font/[name]-[contenthash:8].[ext]'
+              name: "font/[name]-[contenthash:8].[ext]"
             }
           }
         ]
