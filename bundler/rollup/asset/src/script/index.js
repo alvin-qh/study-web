013df2902a7cfc0fba0ecffc3e1b6cf0176ab6c8
瞿昊 <quhao@handaotech.com>
2023-07-31 11:06:36 +0800

    delete old project

--- bundler/rollup/asset/src/script/index.js
+++
@@ -1,8 +0,0 @@
-import '../style/index.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-
-import { join } from 'lodash-es';
-import { textbox } from './lib/component.js';
-
-const $wrapper = document.querySelector('.main');
-$wrapper.appendChild(textbox(join(['Hello', 'World'], ' ')));
