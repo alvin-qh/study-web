aa963ec6d9814d2e29aa024847d46991520131e8
瞿昊 <quhao317@163.com>
2023-12-15 22:30:08 +0800

    [storage] add indexed db demo

--- storage/vite.config.ts
+++ storage/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import eslint from 'vite-plugin-eslint';
+import { VitePWA } from 'vite-plugin-pwa';
 
 /**
  * @type {import('vite').UserConfig}
@@ -17,7 +18,31 @@ export default defineConfig({
     }
   },
   plugins: [
-    eslint()
+    eslint(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Web Storage',
+        description: 'Use PWA make web cache',
+        icons: [
+          {
+            src: '/small-logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/large-logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      }
+    })
   ],
   build: {
     target: 'esnext'
