e238dc936ca107d6adc2d281c02e0881277aa2ef
瞿昊 <quhao317@163.com>
2023-12-15 13:25:23 +0800

    [storage] add indexed db demo

--- storage/src/components/form.ts
+++ storage/src/components/form.ts
@@ -9,7 +9,7 @@ export class Form {
   private readonly _data: FormData;
   private readonly _onSubmit?: (data: FormData) => void;
 
-  constructor(data?: FormData, onSubmit?: (fd: FormData) => void | Promise<void>) {
+  constructor(data: FormData | null, onSubmit?: (fd: FormData) => void | Promise<void>) {
     this._data = data ?? {
       name: '', gender: 'M'
     };
