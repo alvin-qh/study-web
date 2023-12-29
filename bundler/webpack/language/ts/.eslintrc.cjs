863230d23359c3c540148e137d4bbe355b3cdeae
Alvin <quhao317@163.com>
2023-12-29 23:24:47 +0800

    add more eslint rule

--- bundler/webpack/language/ts/.eslintrc.cjs
+++ bundler/webpack/language/ts/.eslintrc.cjs
@@ -31,38 +31,39 @@ module.exports = {
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    project: ['tsconfig.json']
   },
   plugins: [
     'simple-import-sort',
     'prettier'
   ],
   rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/quotes': ['warn', 'single'],
     '@typescript-eslint/semi': ['error', 'always'],
     '@typescript-eslint/space-before-function-paren': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    // '@typescript-eslint/no-explicit-any': 'off',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': ['error', 'never', { js: 'always' }],
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
     indent: ['warn', 2],
     'linebreak-style': ['error', 'unix'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 0 }],
+    'no-plusplus': 'off',
+    'no-trailing-spaces': 'warn',
+    'quote-props': ['error', 'as-needed'],
     quotes: ['warn', 'single'],
     semi: ['error', 'always'],
-    'import/no-extraneous-dependencies': 'off',
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
-    'sort-imports': 'off',
-    'quote-props': ['error', 'as-needed'],
-    'no-multiple-empty-lines': ['warn', { max: 2, maxEOF: 0 }],
-    'no-trailing-spaces': 'warn',
-    'no-plusplus': 'off',
-    'import/prefer-default-export': 'off',
-    'import/no-unresolved': 'off',
-    'import/extensions': ['error', 'never', { js: 'always' }]
+    'sort-imports': 'off'
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
   }
 };
