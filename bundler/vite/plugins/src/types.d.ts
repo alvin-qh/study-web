370a7872719eb368ade9395d7b856e34777e68ca
瞿昊 <quhao317@163.com>
2024-01-05 10:27:47 +0800

    [vite/plugins] update solidjs router component

--- bundler/vite/plugins/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '~solid-pages' {
-  import type { RouteRecordRaw } from 'vue-router';
-
-  const routes: RouteRecordRaw[];
-  export default routes;
-}
--- bundler/vite/plugins/src/vite-env.d.ts
+++ bundler/vite/plugins/src/vite-env.d.ts
@@ -1,3 +1,10 @@
 /// <reference types="vite/client" />
 
 declare module 'vite-plugin-eslint';
+
+declare module '~solid-pages' {
+  import type { type RouteDefinition } from '@solidjs/router';
+
+  const routes: RouteDefinition[];
+  export default routes;
+}
